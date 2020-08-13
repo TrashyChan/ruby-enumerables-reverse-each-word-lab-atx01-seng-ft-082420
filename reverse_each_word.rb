ceb86381a43f63d9fa5fb018101c96da48c4562b
John Lonetree <jlonetree@gmail.com>
2020-08-13 19:04:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -17,3 +17,13 @@ def reverse_using_collect(words)
   end
   new_array.join(" ")
 end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  test_array = []
+  
+  array.collect do|string|
+    test_array << string.reverse
+  end
+  test_array.join(" ")
+end
